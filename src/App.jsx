7c7803beaa93f49ad36e616ea48a7bc4c216522d
Aravind-k <120537950+Aravind-kk@users.noreply.github.com>
2023-01-28 10:44:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Components/Heading";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+
+export default function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Components/Note.jsx
@@ -0,0 +1,16 @@
+var React = require("react");
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Aravind</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sunt aut
+        quisquam quo magnam rem nostrum placeat doloribus alias dolorum. Ratione
+        in beatae pariatur sequi corporis eius iure laboriosam quaerat.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
